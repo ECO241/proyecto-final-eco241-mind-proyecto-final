ce027d185faaf23be1bc9465199c583ad559dc4c
sacuadros09 <sacuma09@gmail.com>
2024-04-27 17:38:27 -0500

    fixed errors

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -15,7 +15,7 @@ export function Register () {
         <h1 className='bot-message'>Register</h1>
         <section className='button-regis'>
         <Button image="/src/assets/mail.png"></Button>
-        <Button url ="/UserRegister" image="/src/assets/person.png"></Button>
+        <Button url ="/UserRegister" image="/src/assets/person.png" title="Sign in with Google"></Button>
         </section>
         <h3 className="account">Already have an account?Sign In</h3>
       </section> 
